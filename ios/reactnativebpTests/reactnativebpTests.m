ab2d6ef2718ce11c44cbe289dcdba0d551aeb19e
faizLaeeq <faizLaeeq@gmail.com>
2023-03-30 14:01:23 +0500

    rename

--- ios/reactnativebpTests/reactnativebpTests.m
+++ ios/reactnativebpTests/reactnativebpTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface veventTests : XCTestCase
+@interface reactnativebpTests : XCTestCase
 
 @end
 
-@implementation veventTests
+@implementation reactnativebpTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
