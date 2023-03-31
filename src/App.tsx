2e6723fdf15ece0167cdf8421237923d04200790
faizLaeeq <faizLaeeq@gmail.com>
2023-03-31 14:25:17 +0500

    added Auth0 wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import {Provider} from 'react-redux';
+import { Auth0Provider } from 'react-native-auth0';
+import { Provider } from 'react-redux';
 import store from './redux/store';
+import Home from './screens/Home';
 
-const App = (): JSX.Element => {
-  return (
-    <Provider store={store}>
-      <SafeAreaView>
-        <View>
-          <Text>Hello World!!!</Text>
-        </View>
-      </SafeAreaView>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <Auth0Provider
+      domain={process.env.AUTH_DOMAIN || ''}
+      clientId={process.env.AUTH_CLIENT_ID || ''}
+    >
+      <Home />
+    </Auth0Provider>
+  </Provider>
+);
 
 export default App;
